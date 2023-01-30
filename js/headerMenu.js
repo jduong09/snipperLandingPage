5bd74ae296c7a795a5d6bf0ec1740d25191aa596
Justin Duong <jduong@umich.edu>
2023-01-29 19:06:48 -0500

    Finished tablet view (768px min) css design.

--- js/headerMenu.js
+++ js/headerMenu.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
   const imgHamburger = document.getElementById('img-hamburger');
   const listMenuNav = document.getElementById('list-menu-nav');
+  const inputSearch = document.getElementById('input-search');
 
   imgHamburger.addEventListener('click', () => {
     if (listMenuNav.classList.contains('hide')) {
@@ -9,4 +10,26 @@ document.addEventListener('DOMContentLoaded', () => {
       listMenuNav.classList.add('hide');
     }
   });
+
+  window.addEventListener('resize', () => {
+    if (window.innerWidth >= 768) {
+      imgHamburger.classList.add('hide');
+      listMenuNav.classList.remove('hide');
+      inputSearch.classList.add('hide');
+    } else {
+      imgHamburger.classList.remove('hide');
+      listMenuNav.classList.add('hide');
+      inputSearch.classList.remove('hide');
+    }
+  });
+
+  if (window.innerWidth >= 768) {
+    imgHamburger.classList.add('hide');
+    listMenuNav.classList.remove('hide');
+    inputSearch.classList.add('hide');
+  } else {
+    imgHamburger.classList.remove('hide');
+    listMenuNav.classList.add('hide');
+    inputSearch.classList.remove('hide');
+  }
 });
